6ad885400999c7d694ca0b1b2052a95cbd8aa6a6
Ty-Cheng5 <tsenopole@gmail.com>
2026-07-12 05:17:06 -0400

    slogan up

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import Navbar from '../components/Navbar';
+import Picture from '../images/picture.png'
 import './Home.css';
 
 function Home() {
@@ -6,17 +7,24 @@ function Home() {
     <div>
       <Navbar />
       <div className ="page">
-        
         <div className="hide">
           <h1>*</h1>
         </div>
-
         <div className="title">
-          <h1>Research,</h1>
+          <div className="hide">
+            <h1>*</h1>
+          </div>
+          <div className="title2">
+            <h1>Bigger Ideas,</h1>
+            <img src={Picture} alt=""></img>
+          </div>
+          <div className="title3">
+            <h1>Backed by Scale</h1>
+          </div>
         </div>
 
         <div className="recent">
-          <div className="recent-title">
+          <div className="recent-header">
             <div>
               <h2>Read the latest</h2>
             </div>
@@ -25,6 +33,31 @@ function Home() {
               <p>See more blog posts</p>
             </div>
           </div>
+          <div className="recent-container">
+            <div className="recent-card">
+              <h1>h</h1>
+              <h1>h</h1>
+              <h1>h</h1>
+            </div>
+            <div className="recent-card">
+              <h1>h</h1>
+              <h1>h</h1>
+              <h1>h</h1>
+            </div>
+            <div className="recent-card">
+              <h1>h</h1>
+              <h1>h</h1>
+              <h1>h</h1>
+            </div>
+          </div>
+          <div className="recent-scroll">
+            <h1>{'<'}</h1>
+            <h1>{'>'}</h1>
+          </div>
+        </div>
+
+        <div className ="vader">
+
         </div>
 
       </div>
